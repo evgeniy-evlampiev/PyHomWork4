98c8dd0b37f24b82727f439e6b2679633f669bfe
eevgeniy <evlevgenij@yandex.ru>
2023-09-16 14:38:10 +0400

    решение задачи 2

--- TASK2.py
+++ TASK2.py
@@ -10,3 +10,12 @@
 # находясь перед некоторым кустом заданной во входном файле грядки.
 
 
+N = int(input('Введи кол-во кустов: '))
+spisok = list(map(int, input(f'Введи {N} цифр через пробел: ').split()))
+
+N = len(spisok)
+spisok = spisok + spisok[:2]
+m = 0
+for i in range(N):
+    m = max( m, spisok[i] + spisok[i+1] + spisok[i+2] )
+print(m)
